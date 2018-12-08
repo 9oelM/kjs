f8ff2c8fe05a5e75c0880f7822a20d036758663e
Joel M <hj923@hotmail.com>
2018-12-08 17:03:42 +0000

    update gulpfile to make it work

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,27 +14,43 @@ const _path = {
   baseDir: '.',
 }
 
-const doNothing = a => a
-
 /**
  * *****************************************
  */
 
-const _lint = ({ source }) =>
-  src(source, { since: lastRun(_lint) })
+function _lint({ source }){
+  return src(source, { since: lastRun(_lint) })
     .pipe(
       tslint({
         configuration: './tslint.json',
         fix: true,
       })
     )
-    .pipe(tslint.report())
+    .pipe(tslint.report({
+            emitError: false
+        }))
     .on('error', err => {
-      doNothing(err)
     })
+}
 
-const _format = ({ source, baseDir }) =>
-  src(source, { since: lastRun(_format) }).pipe(
+function _typescript({ source, destination }){
+  return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(dest(destination))
+}
+
+function _watch({source}){
+  return watch('./src/*.ts', series('lint', 'typescript'));
+}
+
+exports.watch = _watch
+exports.lint = () => _lint(_path)
+exports.typescript = () => _typescript(_path)
+exports.default = _watch
+
+/* Lint used in place
+function _format({ source, baseDir }){
+  return src(source, { since: lastRun(_format) }).pipe(
     typescriptFormatter({
       // Options: https://github.com/vvakame/typescript-formatter/blob/master/lib/cli.ts#L25-L35
       replace: true,
@@ -54,37 +70,8 @@ const _format = ({ source, baseDir }) =>
       //version: boolean;
     })
   )
-
-const _typescript = ({ source, destination }) =>
-  src(source, { since: lastRun(_typescript) })
-    .pipe(tsProject(ts.reporter.nullReporter()))
-    .on('error', err => {
-      console.log(err)
-    })
-    .pipe(ts())
-    .on('error', err => {
-      console.log(err)
-    })
-    .pipe(dest(destination))
-
-const watcher = watch(_path.src)
-
-const _watch = _path => {
-  return watcher.on('change', function(path, stats) {
-    console.log(`File ${path} was changed`)
-    return series(
-      // 1. format
-      () => _format(_path),
-      // 2. tslint
-      () => _lint(_path),
-      // 3. typescript compile
-      () => _typescript(_path)
-    )
-  })
 }
 
-exports.watch = _watch
 exports.format = () => _format(_path)
-exports.lint = () => _lint(_path)
-exports.typescript = () => _typescript(_path)
-exports.default = () => console.log('test')
+
+*/
