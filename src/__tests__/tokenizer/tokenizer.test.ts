1840feff5d2be5a7090086192cad911ec72fda75
Joel M <hj923@hotmail.com>
2019-01-21 13:25:32 +0000

    rename it to .spec.ts

--- src/__tests__/tokenizer/tokenizer.test.ts
+++
@@ -1,8 +0,0 @@
-import Tokenizer from '../../tokenizer/tokenizer'
-
-describe('Test', () => {
-  it('1', () => {
-      const tokenizer = new Tokenizer(' ')
-      expect(tokenizer.isWhitespace(' ')).toEqual(true)
-    })
-})
